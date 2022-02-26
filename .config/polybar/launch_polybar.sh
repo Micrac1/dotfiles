941a51b4b9553a7961c7d088253ef3a8aa6507ca
peter@clamp <micrac.official@gmail.com>
2022-02-26 14:39:52 +0100

    Polybar - reworking of the launching script as well as the config to support multiple bars for different computers

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
+killall polybar 2>/dev/null
 
-killall polybar
-
-if [ "$1" != "-t" ]; then
-	exec >/dev/null 2>&1
+FLAGS="--reload"
+if [ "$1" != "-d" ]; then
+  FLAGS+=" --quiet"
 fi
 
-if type "xrandr" 1>/dev/null ; then
-	if (xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1); then
-		for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
-			MONITOR=$m polybar --reload secondary &
-		done
-		MONITOR=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1) polybar --reload primary &
-	else
-		for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
-			MONITOR=$m polybar --reload primary &
-		done
-	fi
+# Check what computer are we on
+case "${HOSTNAME}" in 
+  "ende"|"clamp")
+    CONFIG="_${HOSTNAME}"
+    ;;
+  *)
+    CONFIG=""
+    ;;
+esac
+
+# check if we have xrandr
+if ! xrandr --version > /dev/null; then
+  echo "[POLYBAR] Error: xrandr not found! Running a single polybar instance..."
+  polybar ${FLAGS} "primary${CONFIG}" &
+  exit
+fi
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+
+# Primary display exists
+if test -n "${PRIMARY}"; then
+  # Launch secondary on every NON-primaray monitor
+  for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+    # launch secondary
+    POLYBAR_MONITOR="${m}" polybar ${FLAGS} "secondary${CONFIG}" &
+  done
+  # launch primary on primary monitor
+  POLYBAR_MONITOR="${PRIMARY}" polybar ${FLAGS} "primary${CONFIG}" &
 else
-  polybar --reload primary &
+  # Launch primary on every connected monitor
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    POLYBAR_MONITOR="${m}" polybar ${FLAGS} "primary${CONFIG}" &
+  done
 fi
