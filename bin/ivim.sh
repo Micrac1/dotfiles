6cceb2afaf2fffcd728dabc23d281ae7dce99ba0
peter@ende <micrac.official@gmail.com>
2023-09-16 02:10:36 +0200

    bin/ivim.sh - fixed whatever garbage it was

--- bin/ivim.sh
+++ bin/ivim.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 SERVERNAME="ivimserver"
-if [ ! -z "${1}" ]; then
-	SERVERNAME="${1}"
-fi
 
-shift
-if vim --serverlist | egrep -i -q "^${SERVERNAME}$"; then
+if vim --serverlist | grep -E -i -q "^${SERVERNAME}$"; then
 	vim --servername "${SERVERNAME}" --remote-tab-silent "$@"
 else
-	xfce4-terminal --disable-server -x vim --servername "${SERVERNAME}" --remote-tab-silent "$@" &
+	xfce4-terminal -x vim --servername "${SERVERNAME}" --remote-tab-silent "$@" &
 fi
