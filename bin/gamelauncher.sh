fa83b70225defaab28c0be487c43cb411c1847d9
peter@clamp <micrac.official@gmail.com>
2024-01-17 01:06:12 +0100

    gamelauncher.sh - fixed unfinished work + changed from zenity to yad (zenity fallback)

--- bin/gamelauncher.sh
+++ bin/gamelauncher.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 LIST=(
-	'~/bin/wp.sh -n mc ; gamemoderun minecraft-launcher' "Minecraft"
-	'~/bin/wp.sh -n bh ; steam -applaunch 291550' "Brawlhalla"
-	'~/bin/wp.sh -n dota ; steam -applaunch 570' "Dota 2"
-	'~/bin/wp.sh -n csgo ; steam -applaunch 730' "CS:GO"
-	'gamemoderun atlauncher' "ATLauncher"
-	'gamemoderun xonotic-sdl' "Xonotic"
-	'gamemoderun supertuxkart' "Super Tux Kart"
+  "Minecraft" '~/bin/wp.sh -n mc ; gamemoderun minecraft-launcher'
+  "Brawlhalla" '~/bin/wp.sh -n bh ; steam -applaunch 291550'
+  "Dota 2" '~/bin/wp.sh -n dota ; steam -applaunch 570'
+  "CS:GO" '~/bin/wp.sh -n csgo ; steam -applaunch 730'
+  "ATLauncher" 'gamemoderun atlauncher'
+  "Xonotic" 'gamemoderun xonotic-sdl'
+  "Super Tux Kart" 'gamemoderun supertuxkart'
 
-	'notify-send test1 ; notify-send test2 ; sleep 2s' "Test"
+  "Test" 'notify-send $HOSTNAME ; notify-send test2 ; sleep 20s'
 )
 
-
-# depracted
 get_game(){
-	echo `zenity --list --title="Launch Game" --text "Launch Game" \
-	--height=480 \
-	--column="command" --column="Game" --hide-column=1 "${LIST[@]}"`  
+  # check if we have yad installed, fall back to zenity
+  DIALOG_CMD=""
+  if [ -x `command -v yad` ]; then
+    DIALOG_CMD="yad"
+  elif [ -x `command -v zenity` ]; then
+    DIALOG_CMD="zenity"
+  else
+    printf "Error: yad and zenity are unavailable. Exiting...\n"
+  fi
+  echo $("${DIALOG_CMD}" --list --separator='' --hide-header \
+    --hide-column=2 --print-column=2 --search-column=1 \
+    --height=480 \
+    --title="Launch Game" --text "Launch Game" \
+    --column="Game" --column="command"  \
+    -- "${LIST[@]}")
 }
 
-run_game(){
-	test -z "${1}" || gamemoderun "${GAME}" &
-}
-
-if [ "$1" = ]; then
-	gui_select
-else
-	eval `get_game` &
-	disown
-fi
-
-
-#GAME=`get_game`
-#run_game "${GAME}"
+GAME="$(get_game)"
+eval "${GAME}" &
