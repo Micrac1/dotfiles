5b38e40cae1eac7df5e4967f6a0281f428d902b8
peter@clamp <micrac.official@gmail.com>
2025-06-07 22:32:26 +0200

    mute.sh - change source to @DEFAULT_SOURCE@

--- .dfs/bin/mute.sh
+++ .dfs/bin/mute.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#pactl set-source-mute @DEFAULT_SOURCE@ toggle
-SOURCE=alsa_input.pci-0000_00_1b.0.analog-stereo
+
 if [ "$1" = "" ]; then
-  pactl set-source-mute "$SOURCE" toggle
+  pactl set-source-mute @DEFAULT_SOURCE@ toggle
 fi
-STATUS=$(pactl list sources | grep "$SOURCE" -m 1 -A6 | grep 'Mute: ' | cut -d' ' -f2)
+STATUS=$(pactl get-source-mute @DEFAULT_SOURCE@ | cut -d ' ' -f2)
 
 if [ "$STATUS" = 'yes' ]; then
-	#notify-send 'Microphone Muted'
 	xset led named "Scroll Lock"
 else
-	#notify-send 'Microphone Unmuted'
 	xset -led named "Scroll Lock"
 fi
 
