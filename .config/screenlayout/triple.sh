4b0325759cf4928bda67c2cceb642fa94aee5338
peter@ende <micrac.official@gmail.com>
2025-07-27 00:19:48 +0200

    screenlayout - update layout

--- .config/screenlayout/triple.sh
+++ .config/screenlayout/triple.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr \
-  --output VGA1 --mode 1280x1024 --rate 75.02 --pos 0x56 \
+  --output DP-4 --mode 1280x1024 --rate 75.02 --pos 0x56 \
   --output HDMI-0 --primary --mode 1920x1080 --rate 75.00 --pos 1280x0 \
   --output DP-1 --auto --pos 3200x0
